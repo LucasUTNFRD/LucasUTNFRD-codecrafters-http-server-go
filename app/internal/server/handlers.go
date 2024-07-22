31bf542959357a586f7d218fbbde41f4c57a70cc
LucasUTNFRD <ldelgado@frd.utn.edu.ar>
2024-07-21 22:56:18 -0300

    codecrafters submit [skip ci]

--- app/internal/server/handlers.go
+++ app/internal/server/handlers.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -43,7 +44,41 @@ func fileHandler(w http.ResponseWriter, r *http.Request, dir string) {
 	log.Println("debugg")
 	fileName := strings.TrimPrefix(r.URL.Path, "/files/")
 	filePath := filepath.Join(dir, fileName)
-	log.Println(filePath)
+
+	switch r.Method {
+	case http.MethodGet:
+		getFile(w, r, filePath)
+	case http.MethodPost:
+		postFile(w, r, filePath)
+	default:
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+	}
+
+}
+
+// TODO: create the file put the data inside and return 201
+func postFile(w http.ResponseWriter, r *http.Request, filePath string) {
+	file, err := os.Create(filePath)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	// Ensure the file is closed when the function returns
+	defer file.Close()
+
+	// Copy the request body into the newly created file
+	_, err = io.Copy(file, r.Body)
+	if err != nil {
+		// If there's an error copying the data, return a 500 Internal Server Error
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	log.Println("aca deberia retornal el status 201")
+	w.WriteHeader(http.StatusCreated)
+
+	fmt.Fprint(w)
+
+}
+func getFile(w http.ResponseWriter, r *http.Request, filePath string) {
 	file, err := os.Open(filePath)
 	if err != nil {
 		http.NotFound(w, r)
@@ -56,6 +91,7 @@ func fileHandler(w http.ResponseWriter, r *http.Request, dir string) {
 		http.NotFound(w, r)
 		return
 	}
+
 	w.Header().Set("Content-Type", "application/octet-stream")
 	w.Header().Set("Content-Length", fmt.Sprintf("%d", stat.Size()))
 	w.WriteHeader(http.StatusOK)
